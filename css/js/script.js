bc51041386f78e1b50d19451b35118b901bafb47
mohammedomary-dev <mohammedomary2005@gmail.com>
2026-02-09 20:19:23 +0300

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,28 @@
 
+let orderList = [];
+
+function addToOrder(item) {
+    orderList.push(item);
+    displayOrder();
+}
+
+function displayOrder() {
+    let list = document.getElementById("orderList");
+    list.innerHTML = "";
+
+    orderList.forEach(function(item) {
+        let li = document.createElement("li");
+        li.textContent = item;
+        list.appendChild(li);
+    });
+}
+
+function placeOrder() {
+    if (orderList.length === 0) {
+        alert("Your order is empty!");
+    } else {
+        alert("Order placed successfully!");
+        orderList = [];
+        displayOrder();
+    }
+}
